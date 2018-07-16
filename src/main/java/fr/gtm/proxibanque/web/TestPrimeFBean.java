a5174e24c102613de7847d054f88e857ce597730
Arno <vallette.arnaud@gmail.com>
2018-07-16 17:52:43 +0200

    JSF bean et SPRING BEAN Link ok ok

--- src/main/java/fr/gtm/proxibanque/web/TestPrimeFBean.java
+++ src/main/java/fr/gtm/proxibanque/web/TestPrimeFBean.java
@@ -4,9 +4,8 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
-
-import org.springframework.beans.factory.annotation.Autowired;
 
 import fr.gtm.proxibanque.business.OperationsBancairesService;
 import fr.gtm.proxibanque.domain.Compte;
@@ -16,15 +15,30 @@ import fr.gtm.proxibanque.domain.Compte;
 public class TestPrimeFBean {
 	private List<Compte> comptes;
 
-	@Autowired
-	OperationsBancairesService service;
+	@ManagedProperty("#{operationsBancairesService}")
+	private OperationsBancairesService service;
 
 	public List<Compte> getComptes() {
 		return this.comptes;
 	}
 
+	/**
+	 * @return the service
+	 */
+	public OperationsBancairesService getService() {
+		return this.service;
+	}
+
 	@PostConstruct
 	public void init() {
 		this.comptes = this.service.findComptes(1);
+	}
+
+	/**
+	 * @param service
+	 *            the service to set
+	 */
+	public void setService(final OperationsBancairesService service) {
+		this.service = service;
 	}
 }
